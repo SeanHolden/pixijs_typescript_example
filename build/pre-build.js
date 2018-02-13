ae9e0214768d87e4ecfefd05756843016e7d79a7
Sean Holden <seanholden1@gmail.com>
2018-02-13 20:15:19 +0000

    First attempt at an OO style

--- build/pre-build.js
+++ build/pre-build.js
@@ -1,6 +1,7 @@
 var fs = require('fs-extra');
 
 fs.copySync('./src/index.html', './dist/index.html');
+fs.copySync('./src/velocity/index.html', './dist/velocity/index.html');
 fs.copySync('./node_modules/requirejs/require.js', './dist/js/lib/require.js');
 fs.copySync('./node_modules/pixi.js/dist/pixi.min.js', './dist/js/lib/pixi.min.js');
 fs.copySync('./node_modules/pixi.js/dist/pixi.min.js.map', './dist/js/lib/pixi.min.js.map');
---
+++ src/velocity/velocity.ts
@@ -0,0 +1,54 @@
+import { Application, loader, Sprite } from "pixijs";
+
+class CustomSprite extends Sprite {
+    velocity: {
+        x: number,
+        y: number
+    };
+}
+
+class Game {
+    private cat: CustomSprite;
+    private app: Application;
+
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.app = new Application({
+            width: 512,
+            height: 512,
+            backgroundColor: 0x061639
+        });
+        
+        document.body.appendChild(this.app.view);
+        this.preload();
+    }
+
+    preload() {
+        loader.add([
+                "../assets/images/cat.png",
+                "../assets/images/tileset.png"
+            ])
+            .load(this.create.bind(this));
+    }
+
+    create() {
+        this.cat = new CustomSprite(
+            loader.resources["../assets/images/cat.png"].texture
+        );
+        this.cat.velocity = { x: 0, y: 0 };
+        this.app.stage.addChild(this.cat);
+        this.app.ticker.add(this.update.bind(this));
+    }
+
+    update(): void {
+        this.cat.velocity.x = 1;
+        this.cat.velocity.y = 1;
+        this.cat.x += this.cat.velocity.x;
+        this.cat.y += this.cat.velocity.y;
+    }
+}
+
+new Game();
